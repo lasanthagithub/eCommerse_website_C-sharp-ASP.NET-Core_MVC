3dd37d74a1e46b09fd57324d545adc4b8a659df0
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-08 15:46:03 -0400

    second class

--- First_program1/First_program1/Program.cs
+++ First_program1/First_program1/Program.cs
@@ -22,3 +22,19 @@ namespace First_program1
 
 // comment line
 // another comment
+
+namespace second_program1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello programmar");
+            Console.ReadLine();
+            Console.WriteLine("Hello, this is a trial");
+            Console.ReadLine();			
+            // //
+            Console.WriteLine("Hello, another trial line");
+        }
+    }
+}
