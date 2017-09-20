5a459043f4e5f08573e39c4634285990a2323d00
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-19 23:10:29 -0400

    Varibles conversion 1

--- First_program1/First_program1/Program.cs
+++ First_program1/First_program1/Program.cs
@@ -14,10 +14,34 @@ namespace First_program1
             string myF_Name = "Lasantha";
             string myL_Name;
             myL_Name = "Rathnayake";
+            bool is_true = false;
 
-            Console.WriteLine("Hello programmar");
+            Console.WriteLine("Hello guys, I'm");
+            Console.Write(myF_Name );
+            Console.WriteLine(" " + myL_Name);
+
+            int myNumber = 1;
+            int myOtherNumber = 5;
+            int answer = myNumber + myOtherNumber;
+            Console.WriteLine(answer);
+
+            // Convert numeric to string
+            string string_Answer = answer.ToString();
+            Console.WriteLine(string_Answer);
+
+            string string_Answer1 = string_Answer;
+            string_Answer1 = string_Answer;
+
+            // Convert numerical strigngs to number
+            Convert.ToInt32(string_Answer1);
+            Console.WriteLine(string_Answer);
+
+            string_Answer1 = string_Answer;
+            Int32.Parse(string_Answer);
+            Console.WriteLine(string_Answer);
+
+
             Console.ReadLine();
-
         }
     }
 }
