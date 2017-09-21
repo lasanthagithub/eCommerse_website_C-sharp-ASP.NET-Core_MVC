8cbb169e5d17f00f25bc1e5f071095a7f5736257
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-20 21:16:19 -0400

    define PetClass and PetStruct

--- PetClassification/PetClassification/Program.cs
+++ PetClassification/PetClassification/Program.cs
@@ -6,10 +6,29 @@ using System.Threading.Tasks;
 
 namespace PetClassification
 {
+    class PetClass
+    {
+        public int Legs;
+        public string Type;
+        public string Name;
+        public bool HasFur;
+    }
+
+    struct PetStruct
+    {
+        public int Legs;
+        public string Type;
+        public string Name;
+        public bool HasFur;
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            PetStruct dog;
+            dog.Type = "dog";
+            Console.ReadLine();
         }
     }
 }
