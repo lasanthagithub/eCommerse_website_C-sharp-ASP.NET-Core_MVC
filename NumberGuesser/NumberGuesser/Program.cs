8e15d55292d9a56a6f3b504f445418bfa8a24c0a
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-21 15:56:10 -0400

    Number guessing : If else and while loop

--- NumberGuesser/NumberGuesser/Program.cs
+++ NumberGuesser/NumberGuesser/Program.cs
@@ -8,8 +8,43 @@ namespace NumberGuesser
 {
     class Program
     {
+        static int GetGuess()
+        {
+            Console.WriteLine("Please enter a number ");
+            string answer = Console.ReadLine();
+
+            // convert string to int
+            int guessNumber = 0;
+            Int32.TryParse(answer, out guessNumber);
+            return guessNumber;
+        }
+
         static void Main(string[] args)
         {
+            int ourNumber = 10;
+            int guessNumber = 0;
+
+            guessNumber = GetGuess();
+
+
+            while (ourNumber != guessNumber)
+            {
+                if (ourNumber == guessNumber)
+                {
+                    Console.WriteLine("That is it: Well done");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry. Not even close");
+
+                }
+            }
+
+
+            Console.WriteLine("Enter any key to trminate");
+
+            Console.ReadLine();
+
         }
     }
 }
