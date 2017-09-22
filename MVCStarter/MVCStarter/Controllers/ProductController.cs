5796adfdb33a89ad96f79bb2b09fe5d67623384d
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-22 13:52:19 -0400

    MVC

---
+++ MVCStarter/MVCStarter/Controllers/ProductController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCStarter.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ProductDetails()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ MVCStarter/MVCStarter/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCStarter.Startup))]
+namespace MVCStarter
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
