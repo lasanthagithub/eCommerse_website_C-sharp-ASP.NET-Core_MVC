cc3362b4aeaa1a5bd0a2754572bdbbb8ed36baa2
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-22 15:58:32 -0400

    eCommerse model setup: Contracts, DAL, Model, WebUI

---
+++ eCommerse/eCommerse.WebUI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="eCommerse.WebUI.MvcApplication" Language="C#" %>
---
+++ eCommerse/eCommerse.WebUI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eCommerse.WebUI.Startup))]
+namespace eCommerse.WebUI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
