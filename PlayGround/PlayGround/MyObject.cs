e69d46f4c87ea6bbe5a1e3a6bf8648a953fe4e16
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-21 22:31:05 -0400

    add new class to namespace PlayGround

---
+++ PlayGround/PlayGround/MyObject.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayGround
+{
+    class MyObject
+    {
+        // inetrnal properties
+        int _number1 = 0;
+        int _number2 = 0;
+
+        //
+        public int number1 { get; set; }
+        public int number2 { get; set; }
+        public int number3 { get; set; }
+
+        // method
+        public int Calculate(int number1, int number2)
+        {
+            return number1 + number2;
+        }
+
+        public string MyMethod()
+        {
+            return "Some value";
+        }
+    }
+}
--- PlayGround/PlayGround/Program.cs
+++ PlayGround/PlayGround/Program.cs
@@ -16,26 +16,4 @@ namespace PlayGround
         }
     }
 
-    class MyObject
-    {
-        // inetrnal properties
-        int _number1 = 0;
-        int _number2 = 0;
-
-        //
-        public int number1 { get; set; }
-        public int number2 { get; set; }
-        public int number3 { get; set; }
-
-        // method
-        public int Calculate(int number1, int number2)
-        {
-            return number1 + number2;
-        }
-
-        public string MyMethod()
-        {
-            return "Some value";
-        }
-    }
 }
