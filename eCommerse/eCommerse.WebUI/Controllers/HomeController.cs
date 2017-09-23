f2e35e88b38f68ac97dcdadd31639b2fe77dd04d
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-23 01:19:01 -0400

    Setup step 2

--- eCommerse/eCommerse.WebUI/Controllers/HomeController.cs
+++ eCommerse/eCommerse.WebUI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using eCommerse.DAL.Data;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,6 +11,8 @@ namespace eCommerse.WebUi.Controllers
     {
         public ActionResult Index()
         {
+            DataContext context = new DataContext();
+
             return View();
         }
 
