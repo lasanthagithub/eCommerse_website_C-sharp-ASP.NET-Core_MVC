b47ddce00554787b0a292d353c2163d3d9c920de
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-26 16:01:45 -0400

    Explore California

---
+++ ExploreCalifornia/ExploreCalifornia/Models/Tour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ExploreCalifornia.Models
+{
+    public class Tour
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }        
+        public int Length { get; set; }
+        public decimal Price { get; set; }        
+        public string Rating { get; set; }
+        public bool IncludesMeals { get; set; }
+    }
+}
