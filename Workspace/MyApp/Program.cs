4072ed532e332e31bd2be2de8f740d657ae6b4b6
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-28 11:06:35 -0400

    ExploreCalifornia: adding data to databases1

---
+++ Workspace/MyApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create product object -use several lines
+
+            //Product product1 = new Product();
+            //product1.ID = 101;
+            //product1.Name = "Red apple";
+            //product1.Price = 1.99M;
+
+            // Create product object -use onlu one line
+            var product1 = new Product { ID = 101, Name = "Red aplle", Price = 1.99M};
+
+            // Serialize the product object to JSON string
+            var jsonString = JsonConvert.SerializeObject(product1);
+
+        }
+    }
+    // Create model
+    class Product
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
