08f443ce34b6f671d8a1b2a0272b49010f4a9598
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-09-28 11:18:52 -0400

    json Serialization and deserialization

--- Workspace/MyApp/Program.cs
+++ Workspace/MyApp/Program.cs
@@ -15,10 +15,19 @@ namespace MyApp
             //product1.Price = 1.99M;
 
             // Create product object -use onlu one line
-            var product1 = new Product { ID = 101, Name = "Red aplle", Price = 1.99M};
+            var product1 = new Product { ID = 101, Name = "Red apple", Price = 1.99M};
 
             // Serialize the product object to JSON string
             var jsonString = JsonConvert.SerializeObject(product1);
+            Console.WriteLine(jsonString);
+
+            // Deseerialize the JSON string back to product object 
+            var product2 = JsonConvert.DeserializeObject<Product>(jsonString);
+            Console.WriteLine($"The prodiut ID is {product2.ID}");
+            Console.WriteLine($"The prodiut name is {product2.Name}");
+            Console.WriteLine($"The prodiut price is {product2.Price}");
+
+            Console.ReadLine();
 
         }
     }
