da2df49c2ecf495b07f5021712d4fda8f15a9f1a
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-03 15:16:05 -0400

    console app

---
+++ Workspace/Modules/Module_1/SeDes/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SeDes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
