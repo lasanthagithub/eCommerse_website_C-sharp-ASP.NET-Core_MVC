034dc356018c63a33d508f914afa9a711e5ba948
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-03 16:55:13 -0400

    javascript

---
+++ Workspace/Modules/Module_1/SeDes/Product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+public class Product
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+}
+
+Product product = new Product();
+
+var productJson = JsonConvert.SerializeObject(product);
+
+var productDeserilize = JsonConvert.DeserializeObject<Product>(productJson);
