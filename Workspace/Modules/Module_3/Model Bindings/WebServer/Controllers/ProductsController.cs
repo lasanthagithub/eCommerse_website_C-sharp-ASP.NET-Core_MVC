48cb71f3bce3232035cbe42c529e3f84216f06d5
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-12 14:31:14 -0400

    Model building2

--- Workspace/Modules/Module_3/Model Bindings/WebServer/Controllers/ProductsController.cs
+++ Workspace/Modules/Module_3/Model Bindings/WebServer/Controllers/ProductsController.cs
@@ -6,24 +6,27 @@ namespace WebServer.Controllers {
     [Route("api/[controller]")]
     public class ProductsController : Controller {
         [HttpGet]
+        // http://localhost:5000/api/products?id=101&name=P101&price=99.99
         public Product Action1([FromQuery]Product product) {
             return product;
         }
 
         [HttpGet("{id}/{name}/{price}")]
+        //http://localhost:5000/api/products/101/P101/99.99
         public Product Action2([FromRoute]Product product) {
             return product;
         }
 
         [HttpGet("{id}/{name}")]
+        //http://localhost:5000/api/products/101/P101?price=99.99
         public Product Action3(Product product) {
             return product;
         }
 
-        // [HttpPost]
-        // public Product Action4([FromForm]Product product) {
-        //     return product;
-        // }
+        [HttpPost]
+        public Product Action4([FromForm]Product product) {
+            return product;
+        }
 
         // [HttpPost]
         // public Product Action5([FromBody]Product product) {
