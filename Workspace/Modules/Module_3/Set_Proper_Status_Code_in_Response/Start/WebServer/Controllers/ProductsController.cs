f02f7196ab3e722bd8bba8bd16a2d25ddead684c
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-12 15:08:36 -0400

    Set clear action status to client2

--- Workspace/Modules/Module_3/Set_Proper_Status_Code_in_Response/Start/WebServer/Controllers/ProductsController.cs
+++ Workspace/Modules/Module_3/Set_Proper_Status_Code_in_Response/Start/WebServer/Controllers/ProductsController.cs
@@ -7,47 +7,112 @@ namespace WebServer.Controllers {
     [Route("api/[controller]")]
     public class ProductsController : Controller {
 
+        
         [HttpGet]
-        public Product[] Get() {
-            return FakeData.Products.Values.ToArray();
+        // before
+        //public Product[] Get() {
+        //    return FakeData.Products.Values.ToArray();
+        //}
+        // after
+        public ActionResult Get() {
+            if (FakeData.Products != null) {
+                return Ok(FakeData.Products.Values.ToArray());
+            } else {
+                return NotFound();
+            }
         }
-
+        
         [HttpGet("{id}")]
-        public Product Get(int id) {
+        // before
+        //public Product Get(int id) {
+        //    if (FakeData.Products.ContainsKey(id))
+        //        return FakeData.Products[id];
+        //    else
+        //        return null;
+        //}
+
+
+        // after
+        public ActionResult Get(int id) {
             if (FakeData.Products.ContainsKey(id))
-                return FakeData.Products[id];
+                return Ok(FakeData.Products[id]);
             else
-                return null;
-        }
+                return NotFound();
+        }   
+
 
         [HttpGet("from/{low}/to/{high}")]
+        //before
         public Product[] Get(int low, int high) {
             var products = FakeData.Products.Values
             .Where(p => p.Price >= low && p.Price <= high).ToArray();
             return products;
         }
-
-        [HttpPost]
-        public Product Post([FromBody]Product product) {
-            product.ID = FakeData.Products.Keys.Max() + 1;
-            FakeData.Products.Add(product.ID, product);
-            return product; // contains the new ID
+        // after
+        public ActionResult Get(int low, int high) {
+            var products = FakeData.Products.Values
+            .Where(p => p.Price >= low && p.Price <= high).ToArray();
+            if (products.Length > 0) { // LINQ guarantees the products won't be null
+                return Ok(products);
+            } else {
+                return NotFound();
+            }
         }
 
+        [HttpPost]
+        // before
+        //public Product Post([FromBody]Product product) {
+        //    product.ID = FakeData.Products.Keys.Max() + 1;
+        //   FakeData.Products.Add(product.ID, product);
+        //    return product; // contains the new ID
+        //}
+        // after
+        public ActionResult Post([FromBody]Product product) {
+            product.ID = FakeData.Products.Keys.Max() + 1;
+            FakeData.Products.Add(product.ID, product);
+            return Created($"api/products/{product.ID}", product); // contains the new ID
+            // This is the concept of HATEOAS (Hypermedia as the Engine of Application State).
+        }
+
+
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody]Product product) {
+        // before
+        //public void Put(int id, [FromBody]Product product) {
+        //    if (FakeData.Products.ContainsKey(id)) {
+        //        var target = FakeData.Products[id];
+        //        target.ID = product.ID;
+        //        target.Name = product.Name;
+        //        target.Price = product.Price;
+        //    }
+        //}
+        // after
+        public ActionResult Put(int id, [FromBody]Product product) {
             if (FakeData.Products.ContainsKey(id)) {
                 var target = FakeData.Products[id];
                 target.ID = product.ID;
                 target.Name = product.Name;
                 target.Price = product.Price;
+                return Ok();
+            } else {
+                return NotFound();
             }
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id) {
+        // before
+        //public void Delete(int id) {
+        //    if (FakeData.Products.ContainsKey(id)) {
+        //        FakeData.Products.Remove(id);
+        //    }
+        //}
+
+        // afetr
+        public ActionResult Delete(int id) {
             if (FakeData.Products.ContainsKey(id)) {
                 FakeData.Products.Remove(id);
+                return Ok();
+            } else {
+                return NotFound();
             }
         }
     }
