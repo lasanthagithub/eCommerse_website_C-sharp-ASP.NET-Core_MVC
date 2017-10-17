9004fdc9357660c8479bc7ef3e50baf9897ab3cd
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-17 10:39:40 -0400

    data access model

---
+++ Workspace/Connect_MySQL_database/WeServer/Models/DataAccess.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MySQL.Data.EntityFrameworkCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WeServer.Models
+{
+    public class SakilaDbContext:DbContext
+    {
+        public SakilaDbContext(DbContextOptions<SakilaDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Film> Film { get; set; }
+
+    }
+
+    public class SakilaDbContextFactory
+    {
+        public static SakilaDbContext Create(string connectionString)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<SakilaDbContext>();
+            optionsBuilder.UseMySQL(connectionString);
+            var sakilaDbContext = new SakilaDbContext(optionsBuilder.Options);
+            return sakilaDbContext;
+        }
+    }
+}
