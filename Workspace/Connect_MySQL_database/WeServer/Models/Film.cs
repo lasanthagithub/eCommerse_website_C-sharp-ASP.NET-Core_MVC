3b6a4b23691c6e9883f28d8076b24664ba5705e7
Lasantha Rathnayake <lasanthakr77@gmail.com>
2017-10-17 10:18:33 -0400

    add film data model

--- Workspace/Connect_MySQL_database/WeServer/Models/Film.cs
+++ Workspace/Connect_MySQL_database/WeServer/Models/Film.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,11 @@ namespace WeServer.Models
 {
     public class Film
     {
+        [Key] // Film_ID is the key
+        public int Film_ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Release_Year { get; set; }
+
     }
 }
